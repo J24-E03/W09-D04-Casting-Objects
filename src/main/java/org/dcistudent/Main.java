d7e87e457c24a7933d1174095d837973dbae711f
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2024-11-28 12:36:18 +0100

    Implement polymorphic array list

--- src/main/java/org/dcistudent/Main.java
+++ src/main/java/org/dcistudent/Main.java
@@ -7,16 +7,21 @@ import org.dcistudent.services.gaming.Chess;
 import org.dcistudent.services.gaming.Game;
 import org.dcistudent.services.gaming.TicTacToe;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         new Main();
     }
 
     public Main() {
+        // Task 1.1
+        System.out.println("--- Task 1.1 ---");
         // Upcasting: Chess object to Game reference
         Game game = new Chess(); // Upcasting
         game.play();
 
+        // Task 1.2
         // Downcasting: Game reference back to Chess object
         if (game instanceof Chess) {
             Chess chess = (Chess) game; // Downcasting
@@ -30,11 +35,21 @@ public class Main {
             System.out.println("Invalid downcasting: Cannot cast Game to TicTacToe.");
         }
 
+        // Task 2.1.{1,2,2} & 2.2.1
+        System.out.println("--- Task 2.1.{1,2,2} & 2.2.1 ---");
         // Upcasting vehicles
         Vehicle car = new Car();
         Vehicle bike = new Bike();
-        ((Vehicle) car).move();
-        ((Vehicle) bike).move();
+        try {
+            ((Vehicle) car).move();
+        } catch (ClassCastException e) {
+            System.out.println("Invalid upcasting: Cannot cast Car to Vehicle.");
+        }
+        try {
+            ((Vehicle) bike).move();
+        } catch (ClassCastException e) {
+            System.out.println("Invalid upcasting: Cannot cast Bike to Vehicle.");
+        }
 
         // Downcasting vehicles
         if (car instanceof Car) {
@@ -52,6 +67,21 @@ public class Main {
             ((Bike) vehicle).ringBell();
         } catch (ClassCastException e) {
             System.out.println("Invalid downcasting: Cannot cast Vehicle to Bike.");
+        }
+
+        // Task 2.3
+        System.out.println("--- Task 2.3 ---");
+        List<Vehicle> vehicles = List.of(new Vehicle(), new Car(), new Bike(), new Vehicle());
+        for (Vehicle v : vehicles) {
+            v.move();
+
+            if (v instanceof Car c) {
+                c.honk();
+            }
+
+            if (v instanceof Bike b) {
+                b.ringBell();
+            }
         }
     }
 }
